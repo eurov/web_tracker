a7012071d4c3d4000622b8cab92425aa410cb3fb
Eugene Egorov <egorovzhenya@gmail.com>
2024-03-15 19:47:51 +0300

    Register routers to endpoints

--- web_tracker/journal/views.py
+++ web_tracker/journal/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.viewsets import GenericViewSet
+
+from web_tracker.journal.models import Visit
+from web_tracker.journal.serializers import LinksSerialazer
+
+
+class DomainsViewSet(ListModelMixin, GenericViewSet):
+
+    def get_queryset(self):
+        time_range = {"from": datetime.min, "to": datetime.max}
+        for k, v in self.request.query_params.items():
+            if k not in time_range:
+                continue
+            time_range[k] = datetime.fromtimestamp(int(v))
+        return Visit.objects.filter(
+            time__gt=time_range["from"], time__lt=time_range["to"]
+        ).distinct()
+
+    def list(self, request):
+        visits = self.get_queryset()
+        if not visits:
+            return Response({"status": "no entries"})
+        return Response(
+            {"domains": visits.values_list("domain", flat=True), "status": "ok"}
+        )
+
+
+class LinksViewSet(CreateModelMixin, GenericViewSet):
+    serializer_class = LinksSerialazer
+
+    def create(self, request):
+        data = request.data.get("links", request.data)
+        serializer = self.serializer_class(data=data, many=isinstance(data, list))
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"status": "ok"})
--- web_tracker/urls.py
+++ web_tracker/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from web_tracker.journal.views import *
+
+
+router = routers.DefaultRouter()
+router.register("visited_domains", DomainsViewSet, basename="domains")
+router.register("visited_links", LinksViewSet, basename="links")
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
 ]
